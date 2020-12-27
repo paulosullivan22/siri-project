f4322c3c999a93e26a7f97c935a7b75d9e828590
Paul O'Sullivan <paulosullivan22@outlook.com>
2020-12-27 16:20:50 +0000

    adding prettier and commencing webworker

---
+++ javascripts/audio.worker.js
@@ -0,0 +1,5 @@
+export async function audioConverter(onMessage, onError) {
+  if (onMessage) {
+    console.log('HERE')
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,130 +1,135 @@
-import * as React from "react"
-
+import * as React from 'react'
+import Helmet from 'react-helmet'
 
 const HomePage: any = () => {
-    const { useEffect } = React
+  const { useEffect } = React
 
-    const recordRef = document.getElementsByClassName('record')
-    const stopRef = document.getElementsByClassName('stop')
-    const soundClips = document.getElementsByClassName('.sound-clips')
+  const recordRef = document.getElementsByClassName('record')
+  const stopRef = document.getElementsByClassName('stop')
 
-    useEffect(() => {
-        if (navigator.mediaDevices.getUserMedia) {
+  useEffect(() => {
+    if (navigator.mediaDevices.getUserMedia) {
+      const constraints = { audio: true }
+      let chunks = []
 
-            // window.gapi.load('auth2', function() {});
-            // console.log(window.gapi?.auth2?.getAuthInstance().signIn())
+      // Add webworker here
+      const workerPath: any = '../../javascripts/audio.worker'
+      const worker = new Worker(workerPath)
 
-            const constraints = {audio: true};
-            let chunks = [];
+      worker.postMessage({ command: 'init ' })
 
-            let onSuccess = function (stream) {
-                const mediaRecorder = new MediaRecorder(stream);
+      console.log(worker)
 
-                recordRef[0].onclick = function() {
-                    mediaRecorder.start();
-                    console.log("recorder started");
-                }
+      let onSuccess = function (stream) {
+        console.log(stream)
+        const mediaRecorder = new MediaRecorder(stream)
 
-                stopRef[0].onclick = function () {
-                    mediaRecorder.stop();
-                    console.log("recorder stopped");
-                    // mediaRecorder.requestData();
-                }
+        // worker.postMessage({ command: 'init', config: { sampleRate: stream } })
 
-                mediaRecorder.onstop = function async (e) {
-                    const clipName = 'unnamed_clip'
+        // worker.postMessage({
+        //   command: 'record',
+        //   buffer: [stream.inputBuffer.getChannelData(0), stream.inputBuffer.getChannelData(1)]
+        // })
 
-                    const clipContainer = document.createElement('article');
-                    const clipLabel = document.createElement('p');
-                    const audio = document.createElement('audio');
-                    const deleteButton = document.createElement('button');
-
-                    clipContainer.classList.add('clip');
-                    audio.setAttribute('controls', '');
-                    deleteButton.textContent = 'Delete';
-                    deleteButton.className = 'delete';
-
-                    if (clipName === null) {
-                        clipLabel.textContent = 'My unnamed clip';
-                    } else {
-                        clipLabel.textContent = clipName;
-                    }
-
-                    clipContainer.appendChild(audio);
-                    clipContainer.appendChild(clipLabel);
-                    clipContainer.appendChild(deleteButton);
-                    // soundClips.appendChild(clipContainer);
-
-                    audio.controls = true;
-                    const blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
-                    chunks = [];
-
-                    try {
-                        let body: any = {
-                            "config": {
-                                // "encoding":"FLAC",
-                                // "sampleRateHertz": 16000,
-                                // "languageCode": "en-US",
-                                // "enableWordTimeOffsets": false
-
-                                "enableAutomaticPunctuation": true,
-                                "encoding": "LINEAR16",
-                                "languageCode": "en-US",
-                                "model": "command_and_search",
-                                "sampleRateHertz": 48000
-                            },
-                            "audio": {
-                                // "content": ""
-                                "content": 'gs://siri-project/test_name_1'
-                            }}
-
-                        const res = fetch(`https://storage.googleapis.com/upload/storage/v1/b/siri-project/o?uploadType=media&name=test_name_1`, {
-                            method: "POST",
-                            headers: {
-                                "Authorization": `Bearer ${process.env.GATSBY_GOOGLE_API_AUTH_TOKEN}`,
-                                'Content-Type': "audio/ogg; codecs=opus"
-                            },
-                            body: blob
-                        })
-
-                        const speechRes = fetch(`https://speech.googleapis.com/v1p1beta1/speech:recognize`, {
-                            method: "POST",
-                            headers: {
-                                'Content-Type': "application/json",
-                                "Authorization": `Bearer ${process.env.GATSBY_GOOGLE_API_AUTH_TOKEN}`,
-
-                            },
-                            body: JSON.stringify(body)
-                        })
-                        console.log(res)
-                        console.log(speechRes)
-
-                    } catch(err) {
-                        console.log(err)
-                    }
-                }
-
-                mediaRecorder.ondataavailable = function (e) {
-                    console.log(e.data)
-                    chunks.push(e.data);
-                }
-            }
-
-            let onError = function (err) {
-                console.log('The following error occured: ' + err);
-            }
-
-            navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+        recordRef[0].onclick = function () {
+          mediaRecorder.start()
+          console.log('recorder started')
         }
-    })
-    return (
-        <>
-            <button className={'record'}>Record</button>
-            <button className={'stop'}>Stop</button>
-            <section className={"sound-clips"}>
-            </section>
-            </>
-    )
+
+        stopRef[0].onclick = function () {
+          mediaRecorder.stop()
+          console.log('recorder stopped')
+        }
+
+        mediaRecorder.onstop = async function (e) {
+          console.log(chunks)
+          const blob = new Blob(chunks, { type: 'audio/wav' })
+          chunks = []
+          console.log(blob)
+
+          // worker.postMessage({
+          //   command: 'exportWAV',
+          //   type: 'audio/wav'
+          // })
+
+          function getBase64EncodedAudio(blob: any) {
+            return new Promise<any>((resolve: any, reject: any) => {
+              const reader = new FileReader()
+              let base64data: any = null
+              reader.readAsDataURL(blob)
+              reader.onloadend = () => {
+                base64data = reader.result
+                resolve(base64data)
+              }
+              return base64data
+            })
+          }
+
+          const base64data: any = await getBase64EncodedAudio(blob)
+          const formattedBase64Data: any = base64data.split(',')[1]
+
+          let body: any = {
+            config: {
+              // "encoding":"FLAC",
+              // "sampleRateHertz": 16000,
+              // "languageCode": "en-US",
+              // "enableWordTimeOffsets": false
+
+              enableAutomaticPunctuation: true,
+              encoding: 'LINEAR16',
+              languageCode: 'en-US',
+              model: 'command_and_search',
+              sampleRateHertz: 48000
+            },
+            audio: {
+              content: formattedBase64Data
+              // "uri": 'gs://siri-project/test_name_1'
+            }
+          }
+
+          // const res = fetch(
+          //   `https://storage.googleapis.com/upload/storage/v1/b/siri-project/o?uploadType=media&name=test_name_1`,
+          //   {
+          //     method: 'POST',
+          //     headers: {
+          //       Authorization: `Bearer ${process.env.GATSBY_GOOGLE_API_AUTH_TOKEN}`,
+          //       'Content-Type': 'audio/ogg; codecs=opus'
+          //     },
+          //     body: blob
+          //   }
+          // )
+
+          const speechRes = fetch(`https://speech.googleapis.com/v1p1beta1/speech:recognize`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${process.env.GATSBY_GOOGLE_API_AUTH_TOKEN}`
+            },
+            body: JSON.stringify(body)
+          })
+          console.log(speechRes)
+        }
+
+        mediaRecorder.ondataavailable = function (e) {
+          chunks.push(e.data)
+        }
+      }
+
+      let onError = function (err) {
+        console.log('The following error occured: ' + err)
+      }
+
+      navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError)
+    }
+  })
+  return (
+    <>
+      <Helmet></Helmet>
+      <button className={'record'}>Record</button>
+      <button className={'stop'}>Stop</button>
+      <section className={'sound-clips'}></section>
+    </>
+  )
 }
 
 export default HomePage
