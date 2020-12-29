8abcc41f3ccb41427b6d71a0ee858fc9418fe205
Paul O'Sullivan <paulosullivan22@outlook.com>
2020-12-29 19:23:47 +0000

    testing wit for speech recognition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,20 +8,20 @@ const HomePage: any = () => {
   const stopRef = document.getElementsByClassName('stop')
 
   useEffect(() => {
-    if (navigator.mediaDevices.getUserMedia) {
-      var mediaConstraints = {
+    if (navigator.mediaDevices.getUserMedia && navigator.getUserMedia) {
+      const mediaConstraints = {
         audio: true
       }
 
       navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError)
 
       function onMediaSuccess(stream) {
-        var mediaRecorder = new MediaStreamRecorder(stream)
+        const mediaRecorder = new MediaStreamRecorder(stream)
         mediaRecorder.audioChannels = 1
-        mediaRecorder.mimeType = 'audio/wav' // check this line for audio/wav
+        mediaRecorder.mimeType = 'audio/wav'
 
         recordRef[0].onclick = function () {
-          mediaRecorder.start()
+          mediaRecorder.start(10000)
           console.log('recorder started')
         }
 
@@ -31,9 +31,6 @@ const HomePage: any = () => {
         }
 
         mediaRecorder.ondataavailable = async function (blob) {
-          console.log('DATA AVAILABLE')
-          console.log(blob)
-
           function getBase64EncodedAudio(blob: any) {
             return new Promise<any>((resolve: any, reject: any) => {
               const reader = new FileReader()
@@ -55,8 +52,7 @@ const HomePage: any = () => {
               enableAutomaticPunctuation: true,
               encoding: 'LINEAR16',
               languageCode: 'en-US',
-              model: 'command_and_search',
-              sampleRateHertz: 16000
+              model: 'command_and_search'
             },
             audio: {
               content: formattedBase64Data
@@ -75,15 +71,45 @@ const HomePage: any = () => {
           //   }
           // )
 
-          const speechRes = fetch(`https://speech.googleapis.com/v1p1beta1/speech:recognize`, {
+          // let speechResponse = null
+          // await fetch(`https://speech.googleapis.com/v1p1beta1/speech:recognize`, {
+          //   method: 'POST',
+          //   headers: {
+          //     'Content-Type': 'application/json',
+          //     Authorization: `Bearer ${process.env.GATSBY_GOOGLE_API_AUTH_TOKEN}`
+          //   },
+          //   body: JSON.stringify(body)
+          // })
+          //   .then((res) => res.json())
+          //   .then((res) => (speechResponse = res))
+          // console.log(speechResponse)
+          //
+          // const speech = speechResponse.results[0].alternatives[0].transcript
+          // const encodedSpeech = encodeURIComponent(speech)
+          // const uri = 'https://api.wit.ai/message?v=20200513&q=' + encodedSpeech
+          // const auth = 'Bearer ' + process.env.GATSBY_WIT_TOKEN
+          // fetch(uri, { headers: { Authorization: auth } })
+          //   .then((res) => res.json())
+          //   .then((res) => console.log(res))
+
+          const uri = 'https://api.wit.ai/speech'
+          const auth = 'Bearer ' + process.env.GATSBY_WIT_TOKEN
+          fetch(uri, {
             method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${process.env.GATSBY_GOOGLE_API_AUTH_TOKEN}`
-            },
-            body: JSON.stringify(body)
+            headers: { Authorization: auth, 'Content-type': 'audio/wav' },
+            body: formattedBase64Data
           })
-          console.log(speechRes)
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+
+          // const res = fetch('https://api.wit.ai/speech?client=chromium&lang=en-us&output=json', {
+          //   method: 'GET',
+          //   headers: {
+          //     Accept: 'application/vnd.wit.20160202+json',
+          //     Authorization: `Bearer ${process.env.GATSBY_WIT_TOKEN}`,
+          //     'Content-Type': 'audio/wav'
+          //   }
+          // })
         }
       }
 
