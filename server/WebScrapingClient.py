451595dddb1be866cc70aedb8ce420418fc9c967
Paul O'Sullivan <paulosullivan22@outlook.com>
2021-01-28 20:01:59 +0000

    removing unnecessary comments in web scraping client

---
+++ server/WebScrapingClient.py
@@ -0,0 +1,23 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = 'https://google.com/search?q=Where+can+I+get+the+best+coffee'
+
+# Perform the request
+request = urllib.request.Request(url)
+
+# Set a normal User Agent header, otherwise Google will block the request.
+request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
+raw_response = urllib.request.urlopen(request).read()
+
+html = raw_response.decode("utf-8")
+
+soup = BeautifulSoup(html, 'html.parser')
+
+divs = soup.select("#search div.g")
+for div in divs:
+    results = div.select("h3")
+
+    if (len(results) >= 1):
+        h3 = results[0]
+        print(h3.get_text())
--- server/server.py
+++ server/server.py
@@ -4,8 +4,8 @@ import requests
 import os
 from dotenv import load_dotenv
 
+import WebScrapingClient
 from SpeechToTextClient import SpeechToTextClient
-from LangProcessingClient import LangProcessingClient
 
 load_dotenv()
 
@@ -19,21 +19,18 @@ def audio():
 
     # Instantiating clients
     speech_to_text_client = SpeechToTextClient()
-#     language_processing_client = LangProcessingClient()
 
     encoded_audio_file = request.data
     transcribed_audio = speech_to_text_client.make_recognize(encoded_audio_file)
 
-#     document = language_processing_client.make_document(transcribed_audio)
-#     sentiment = language_processing_client.make_processing(document)
-
     return {
         "transcribed_audio": transcribed_audio,
-#         "sentiment": sentiment
     }
 
 if __name__ == '__main__':
    app.run()
+
+
 
 # source virtualenv/bin/activate
 # FLASK_APP=server.py FLASK_ENV=development flask run
